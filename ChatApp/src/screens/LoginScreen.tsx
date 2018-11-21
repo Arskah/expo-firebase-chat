74c2bcca68cd537ab95886c806b096f69fe14bc4
Aarni Halinen <aarni.halinen@aalto.fi>
2018-11-21 14:15:21 +0200

    Comment out non created components

--- ChatApp/src/screens/LoginScreen.tsx
+++ ChatApp/src/screens/LoginScreen.tsx
@@ -1,17 +1,17 @@
 import React, {Component} from 'react';
-import Logo from '../components/Login/Logo';
-import FormLogin from '../components/Login/FormLogin';
+// import Logo from '../components/Login/Logo';
+// import FormLogin from '../components/Login/FormLogin';
 import Wallpaper from '../components/Login/Wallpaper';
-import ButtonLogin from '../components/Login/ButtonLogin';
+// import ButtonLogin from '../components/Login/ButtonLogin';
 import SignupSection from '../components/Login/SignupSection';
 
 export default class LoginScreen extends Component {
   render() {
     return (
       <Wallpaper>
-			  <Logo />
-			  <FormLogin />
-			  <ButtonLogin />
+			  {/* <Logo /> */}
+			  {/* <FormLogin /> */}
+			  {/* <ButtonLogin /> */}
         <SignupSection />
       </Wallpaper>
     );
