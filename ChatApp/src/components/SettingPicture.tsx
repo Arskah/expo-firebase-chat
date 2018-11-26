63f41d4c7ab0ba3ab5bb67672c1f21702498080a
Tommi <tommi.askola@outlook.com>
2018-11-26 18:48:42 +0200

    Added functionality to change profile picture from gallery or camera

--- ChatApp/src/components/SettingPicture.tsx
+++ ChatApp/src/components/SettingPicture.tsx
@@ -1,26 +1,90 @@
 import * as React from "react";
 import Layout from '../constants/Layout';
 import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
-import { Asset } from 'expo'
+import { Asset, ImagePicker, Permissions } from 'expo'
+import default_image from '../../assets/images/robot-dev.png'
+import Dialog from 'react-native-dialog';
 
-export interface SettingPictureProps {}
-export interface SettingPictureState {}
+export interface SettingPictureProps {
+  image : string
+}
+export interface SettingPictureState {
+  image : string,
+  dialogVisible: boolean
+}
 
 class SettingPicture extends React.Component<SettingPictureProps, SettingPictureState> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      image: props.image || "",
+      dialogVisible: false
+    };
+  }
+
+  showDialog = () => {
+    this.setState({ dialogVisible: true });
+  };
+
+  handleCancel = () => {
+    this.setState({ dialogVisible: false});
+  };
+
+  pickFromGallery = async () => {
+    this.setState({ dialogVisible: false});
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+    });
+
+    console.log(result);
+    if(!result.cancelled){this.setState({image: result.uri})}
+  };
+
+
+  pickFromCamera = async () => {
+    this.setState({ dialogVisible: false});
+    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    if (status === 'granted') {
+      let result = await ImagePicker.launchCameraAsync({
+        allowsEditing: true,
+        aspect: [4, 3],
+      });
+  
+      console.log(result);
+      if(!result.cancelled){this.setState({image: result.uri})}
+    }
+
+  };
+
 
   render() {
 
     return (
-      <TouchableOpacity
-      style={styles.container} 
-      onPress={() => alert("Change picture")}>
-      <Image
-          source={require('../../assets/images/robot-dev.png')}
-          style={styles.image}
-        />
-    </TouchableOpacity> 
+      <View>
+        <TouchableOpacity
+          style={styles.container} 
+          onPress={() => this.showDialog()}>
+          {this.displayImage()}
+        </TouchableOpacity> 
+        <Dialog.Container visible={this.state.dialogVisible}>
+          <Dialog.Title>Pick new picture from</Dialog.Title>
+          <Dialog.Button label="Gallery" onPress={this.pickFromGallery} />
+          <Dialog.Button label="Camera" onPress={this.pickFromCamera} />
+          <Dialog.Button label="Cancel" onPress={this.handleCancel} />
+        </Dialog.Container>
+      </View>
     );
   }
+
+  displayImage() {
+    if (this.state.image === "") {
+      return <Image source={default_image} style={styles.image} />
+    } else {
+      return <Image source={{ uri: this.state.image }} style={styles.image} />
+    }
+}
 
 }
 const DEVICE_WIDTH = Layout.window.width;
@@ -33,7 +97,9 @@ const styles = StyleSheet.create({
   image: {
     resizeMode: 'cover',
     justifyContent: 'center',
-    alignSelf: "center"
+    alignSelf: "center",
+    width: 150,
+    height: 150
   },
 });
 
