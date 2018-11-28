8f5b42b537ab23b0d464ad2cb42116b35e9cd59f
Aarni Halinen <aarni.halinen@aalto.fi>
2018-11-28 17:11:22 +0200

    Add go back to SignupScreen

--- ChatApp/src/screens/SignupScreen.tsx
+++ ChatApp/src/screens/SignupScreen.tsx
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
+import {BackHandler} from 'react-native';
 import Wallpaper from '../components/Wallpaper';
 import SignupForm from '../components/SignupForm'
 
 export default class SignupScreen extends Component {
+  componentDidMount() {
+    this.backHandler = BackHandler.addEventListener('hardwareBackPress', () => {
+      this.props.navigation.navigate('LoginScreen');
+      return true;
+    });
+  }
+
+  componentWillUnmount() {
+    this.backHandler.remove();
+  }
+
 	render() {
 		return (
       <Wallpaper>
