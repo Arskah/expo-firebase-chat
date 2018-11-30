dc87bf2fe2d73ac93e2d196da43880b3eca4518b
Aarni Halinen <aarni.halinen@aalto.fi>
2018-11-30 15:01:27 +0200

    Chat send message sketch

--- ChatApp/src/Fire.ts
+++ ChatApp/src/Fire.ts
@@ -2,6 +2,9 @@ import firebase from "firebase";
 import { Alert } from "react-native";
 import { ENV } from "../environment";
 
+let fb_app = undefined;
+let fb_db = undefined;
+
 export const init = () => {
   const config = {
     apiKey: ENV.APIKEY,
@@ -11,7 +14,8 @@ export const init = () => {
     storageBucket: ENV.SBUCKET,
     messagingSenderId: ENV.SEND_ID,
   };
-  firebase.initializeApp(config);
+  fb_app = firebase.initializeApp(config);
+  fb_db = firebase.database();
 };
 
 // Create new chatroom
@@ -26,12 +30,24 @@ export const chat_add = (chat_id, user_id) => {
 
 // Send a message in chat
 // reference images with some funky syntax
-export const chat_send = (chat_id, message) => {
-  return;
+export const chat_send = (chat_id, message, author) => {
+  let postData = {
+    author: author,
+    message: message,
+  };
+  let new_key = firebase.database().ref().child("messages").push().key;
+  let updates = {};
+  updates[`/chats/${chat_id}/lastMessage/`] = `${author}: ${message}`;
+  updates[`/messages/${chat_id}/${new_key}/`] = postData;
+  return firebase.database().ref().update(updates);
 };
 
 // Leave chatroom
 export const chat_leave = (chat_id) => {
+  return;
+};
+
+export const get_chat_message = (chat_id) => {
   return;
 };
 
@@ -71,7 +87,11 @@ export const user_state_change = (callback) => {
 // true on success + false on fail
 // TODO: Possible need to add callback as parameter for redirections etc.
 export const user_login = (username, passwd) => {
-  return;
+  user_search(username).then((user_profile) => {
+    if (user_profile) {
+      user_login_email(user_profile.email, passwd);
+    }
+  });
 };
 
 export const user_login_email = (email, passwd) => {
@@ -84,8 +104,9 @@ export const user_login_email = (email, passwd) => {
 };
 
 // results
-export const user_search = (search_term) => {
-  return;
+export const user_search = async (search_term) => {
+  const user = "";
+  return user;
 };
 
 // value
